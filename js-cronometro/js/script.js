75b72fcebacfe63d22991d51721a747df2996ae6
WalterCorsini <corsini.walter@gmail.com>
2024-04-10 15:15:23 +0200

    add html,class disabled and add e remove classe disable when click button start and stop

--- js-cronometro/js/script.js
+++ js-cronometro/js/script.js
@@ -1,1 +1,17 @@
-alert("ciao");
+let secondi = 0
+let minuti = 0
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+console.log(startBtn,stopBtn);
+startBtn.addEventListener("click", function(){
+    startBtn.classList.add("disabled");
+    stopBtn.classList.remove("disabled");
+    const stop = setInterval(function(){
+        console.log("ciao");
+    },1000);
+    stopBtn.addEventListener("click", function(){
+        clearInterval(stop);
+        startBtn.classList.remove("disabled");
+        stopBtn.classList.add("disabled");
+    });
+});
