76b5ad04a541de096688b25bb9fa72ebc834519c
WalterCorsini <corsini.walter@gmail.com>
2024-04-10 15:19:06 +0200

    add comments

--- js-cronometro/js/script.js
+++ js-cronometro/js/script.js
@@ -1,17 +1,23 @@
+// INPUT
+// declaration variable
 let secondi = 0
 let minuti = 0
 const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
 console.log(startBtn,stopBtn);
-startBtn.addEventListener("click", function(){
-    startBtn.classList.add("disabled");
-    stopBtn.classList.remove("disabled");
-    const stop = setInterval(function(){
-        console.log("ciao");
-    },1000);
-    stopBtn.addEventListener("click", function(){
-        clearInterval(stop);
-        startBtn.classList.remove("disabled");
-        stopBtn.classList.add("disabled");
+
+// LOGIC
+
+
+startBtn.addEventListener("click", function(){      // add event on click start button
+    startBtn.classList.add("disabled");             // add class disabled start button 
+    stopBtn.classList.remove("disabled");           // remove class disable stop button
+    const stop = setInterval(function(){            // assign set interval to variable and start setInterval
+        console.log("ciao");                        
+    },1000);                                        // 1 second inteval
+    stopBtn.addEventListener("click", function(){   // add event on click stop button
+        clearInterval(stop);                        // stop set interval
+        startBtn.classList.remove("disabled");      // remove start button class disabled
+        stopBtn.classList.add("disabled");          // add stop button class disabled
     });
 });
